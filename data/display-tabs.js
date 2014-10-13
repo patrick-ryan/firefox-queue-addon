a7b5a56d9ba2713014f7d3388579df89aff4e7ea
Patrick Ryan <pryan@virginia.edu>
2014-10-12 22:42:18 -0500

    clean up, and ui upgrade

--- data/display-tabs.js
+++ data/display-tabs.js
@@ -5,8 +5,11 @@
 self.port.on("show", function(data) {
     var title = data[0];
     var url = data[1];
-    console.log("Showing: ", title);
+    // console.log("Showing: ", title);
     var elem = document.createElement("a");
+    var cls = document.createAttribute("class");
+    cls.value = "item";
+    elem.setAttributeNode(cls);
     var href = document.createAttribute("href");
     // href.value = url;
     elem.setAttributeNode(href);
@@ -14,19 +17,9 @@ self.port.on("show", function(data) {
     elem.appendChild(text);
     elem.addEventListener("click", function(event) {
         event.preventDefault();
-        console.log("Entry clicked: ", url);
+        // console.log("Entry clicked: ", url);
         self.port.emit("entry-clicked", url);
         self.port.emit("hide");
     });
     document.body.appendChild(elem);
 });
-
-// self.port.on("end", function() {
-//     self.port.emit("hide");
-// });
-
-// self.port.on("scratch", function() {
-//     while (document.body.firstChild) {
-//         document.body.removeChild(document.body.firstChild);
-//     }
-// });
