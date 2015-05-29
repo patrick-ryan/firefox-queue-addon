8202f4c187c6cc85ba3f04f74f9efd80a9addf18
Patrick Ryan <pryan@virginia.edu>
2015-05-28 23:05:17 -0500

    updated script runner with command

--- addon-dev.sh
+++ addon-dev.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-FF_DIR=~/.mozilla/firefox/
+# cfx run -b ~/Builds/firefox/firefox-bin
+FF_DIR=~/Builds/firefox/
 DEFAULT_PROFILE=`ls ${FF_DIR} | grep .default`
 cfx run --profiledir=${FF_DIR}${DEFAULT_PROFILE}
