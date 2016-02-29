ad9908e310c98ca61dcd1c56f475445d69b7b781
Patrick Ryan <pmr3mh@virginia.edu>
2016-02-29 00:31:30 -0600

    uhh

--- addon-base.sh
+++ addon-base.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 FF_DIR=~/.mozilla/firefox/
 DEFAULT_PROFILE=`ls ${FF_DIR} | grep .default`
-cfx run --profiledir=${FF_DIR}${DEFAULT_PROFILE}
+jpm run --profile=${FF_DIR}${DEFAULT_PROFILE}
--- addon-dev.sh
+++ addon-dev.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# cfx run -b ~/Builds/firefox/firefox-bin
 FF_DIR=~/Builds/firefox/
 DEFAULT_PROFILE=`ls ${FF_DIR} | grep .default`
-cfx run --profiledir=${FF_DIR}${DEFAULT_PROFILE}
+jpm run --profile=${FF_DIR}${DEFAULT_PROFILE}
